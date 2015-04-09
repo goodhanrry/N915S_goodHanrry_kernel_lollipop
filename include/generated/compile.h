04aa42f5cb69786c2a19293c6cb7c218bce7e4b0
goodhanrry <hanrry@21cn.com>
2015-04-09 16:26:47 +0800

    kk-update1-v30

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 SMP PREEMPT Sat Mar 14 22:29:09 CST 2015"
+#define LINUX_COMPILE_BY "hanrry"
+#define LINUX_COMPILE_HOST "hanrry-X201E"
+#define LINUX_COMPILER "gcc version 4.9.2 20141013 (prerelease) (crosstool-NG linaro-1.13.1+bzr2700 - Linaro GCC 2014.10) "
